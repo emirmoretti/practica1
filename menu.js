72d6e421c8975079a93b6e884cbfd3a34c77a637
emirmoretti <71144640+emirmoretti@users.noreply.github.com>
2021-02-16 22:09:16 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,6 @@
+let btnmenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu');
+
+btnmenu.addEventListener('click', function(){
+    menu.classList.toggle('mostrar');
+})
